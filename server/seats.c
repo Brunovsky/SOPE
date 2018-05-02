ee5232d0f28d8c8130e2c21114fd9e274bc34e61
Brunovsky <elbrunovsky@gmail.com>
2018-05-02 12:51:50 +0100

    Rewrote seats.c according to the specs

--- server/seats.c
+++ server/seats.c
@@ -6,36 +6,43 @@
 #include <pthread.h>
 #include <stdbool.h>
 
+#define NO_CLIENT_CODE -1
+
 typedef pthread_mutex_t mutex_t;
 
-typedef struct {
+struct seat_descriptor {
     int num;
     int client; // pid
     bool reserved;
-    mutex_t mutex;
-} seat_t;
+};
+
+typedef struct seat_descriptor Seat;
+typedef struct seat_descriptor seat_t;
 
 static bool seats_initialised = false;
 static bool seats_atexit_set = false;
 static seat_t* seats = NULL;
+static mutex_t* mutexes = NULL;
 static int seats_reserved = 0, seats_size = 0;
 
-static void free_mutexes() {
+static void destroy_mutexes() {
     for (int i = 0; i < seats_size; ++i) {
-        pthread_mutex_destroy(&seats[i].mutex);
+        pthread_mutex_destroy(&mutexes[i]);
     }
 }
 
-static void init_mutexes() {
+static void init_mutexes() {    
     for (int i = 0; i < seats_size; ++i) {
-        pthread_mutex_init(&seats[i].mutex, NULL);
+        pthread_mutex_init(&mutexes[i], NULL);
     }
 }
 
 static void free_seats() {
     if (seats_initialised) {
-        free_mutexes();
-        free((void*)seats);
+        destroy_mutexes();
+        free(mutexes);
+        mutexes = NULL;
+        free(seats);
         seats = NULL;
         seats_reserved = seats_size = 0;
         seats_initialised = false;
@@ -43,20 +50,22 @@ static void free_seats() {
 }
 
 static void init_seats() {
+    seats = malloc(o_seats * sizeof(seat_t));
+    seats_size = o_seats;
+
     for (int i = 0; i < seats_size; ++i) {
         seats[i].num = i + 1;
         seats[i].client = NO_CLIENT_CODE;
         seats[i].reserved = false;
     }
 
+    mutexes = malloc(o_seats * sizeof(seat_t));
     init_mutexes();
 }
 
 void setup_seats() {
-    if (seats_initialised) free_seats(); // assume we come from a fork
+    if (seats_initialised) free_seats();
 
-    seats = malloc(o_seats * sizeof(seat_t));
-    seats_size = o_seats;
     init_seats();
 
     if (!seats_atexit_set) {
@@ -65,85 +74,129 @@ void setup_seats() {
     }
 }
 
-static void lock(int seat_num) {
+static inline void lock_seat(Seat* seats, int seat_num) {
     int i = seat_num - 1;
     pthread_mutex_lock(&seats[i].mutex);
 }
 
-static void unlock(int seat_num) {
+static inline void unlock_seat(Seat* seats, int seat_num) {
     int i = seat_num - 1;
     pthread_mutex_unlock(&seats[i].mutex);
 }
 
-static void free_seat(int seat_num) {
+/**
+ * REQUIRED
+ * @param  seats    [description]
+ * @param  seat_num [description]
+ * @return          [description]
+ */
+static int isSeatFree(Seat* seats, int seat_num) {
+    // the entry point is is_seat_free, which validates seat_num and
+    // redundantly passes in variable seats which is a global in this unit.
+    
+    lock_seat(seats, seat_num);
+
     int i = seat_num - 1;
-    seats[i].client = NO_CLIENT_CODE;
-    seats[i].reserved = false;
-    --seats_reserved;
+    bool b = seats[i].reserved;
+
+    int ret = b;
+
+    DELAY();
+
+    unlock_seat(seats, seat_num);
+
+    return ret;
 }
 
-static void reserve_seat(int seat_num, int client_id) {
+/**
+ * REQUIRED
+ * @param seats    [description]
+ * @param seat_num [description]
+ */
+static void bookSeat(Seat* seats, int seat_num) {
+    // the entry point is book_seat, which validates seat_num and
+    // redundantly passes in variable seats which is a global in this unit.
+    
+    lock_seat(seats, seat_num);
+
     int i = seat_num - 1;
-    seats[i].client = client_id;
-    seats[i].reserved = true;
-    ++seats_reserved;
+    bool b = seats[i].reserved;
+
+    int ret = 0;
+
+    if (b) {
+        ret = SEATS_ERR_SEAT_IS_RESERVED;
+    } else {
+        // Effectively book seat
+        int i = seat_num - 1;
+        seats[i].client = client_id;
+        seats[i].reserved = true;
+    }
+
+    DELAY();
+
+    unlock_seat(seats, seat_num);
+
+    //return ret;
 }
 
-static bool is_reserved(int seat_num) {
+/**
+ * REQUIRED
+ * @param seats    [description]
+ * @param seat_num [description]
+ */
+static void freeSeat(Seat* seats, int seat_num) {
+    // the entry point is free_seat, which validates seat_num and
+    // redundantly passes in variable seats which is a global in this unit.
+    
+    lock_seat(seats, seat_num);
+
     int i = seat_num - 1;
-    return seats[i].reserved;
+    bool b = seats[i].reserved;
+
+    int ret = 0;
+
+    if (b) {
+        ret = SEATS_ERR_SEAT_NOT_RESERVED;
+    } else {
+        // Effectively free seat
+        int i = seat_num - 1;
+        seats[i].client = NO_CLIENT_CODE;
+        seats[i].reserved = false;
+    }
+
+    DELAY();
+
+    unlock_seat(seats, seat_num);
+
+    //return ret;
 }
 
+// Entry point for isSeatFree()
 bool is_seat_free(int seat_num) {
     if (seat_num < 1 || seat_num > o_seats) {
         return SEATS_ERR_INVALID_SEATNUM;
     }
 
-    return is_reserved(seat_num);
+    return isSeatFree(seats, seat_num);
 }
 
+// Entry point for bookSeat()
 int book_seat(int seat_num, int client_id) {
     if (seat_num < 1 || seat_num > o_seats) {
         return SEATS_ERR_INVALID_SEATNUM;
     }
 
-    lock(seat_num);
-    int ret = 0;
-
-    if (is_reserved(seat_num)) {
-        ret = SEAT_ERR_SEAT_RESERVED;
-    } else {
-        reserve_seat(seat_num, client_id);
-    }
-
-    DELAY();
-    unlock(seat_num);
-    return ret;
+    bookSeat(seats, seat_num, client_id);
+    return 0;
 }
 
+// Entry point for freeSeat()
 int free_seat(int seat_num) {
     if (seat_num < 1 || seat_num > o_seats) {
         return SEATS_ERR_INVALID_SEATNUM;
     }
 
-    lock(seat_num);
-    int ret = 0;
-
-    if (is_reserved(seat_num)) {
-        free_seat(seat_num, client_id);
-    } else {
-        ret = SEAT_ERR_SEAT_NOT_RESERVED;
-    }
-
-    DELAY();
-    unlock(seat_num);
-    return ret;
-}
-
-bool empty_house() {
-    return seats_reserved == 0;
-}
-
-bool full_house() {
-    return seats_reserved == seats_size;
+    freeSeat(seats, seat_num);
+    return 0;
 }
--- server/seats.h
+++ server/seats.h
@@ -3,8 +3,6 @@
 
 #include <stdbool.h>
 
-#define NO_CLIENT_CODE -1
-
 #define ERR_MAX -1
 #define ERR_NST -2
 #define ERR_IID -3
@@ -13,43 +11,15 @@
 #define ERR_FUL -6
 
 #define SEATS_ERR_INVALID_SEATNUM    1
-#define SEATS_ERR_SEAT_RESERVED      2
+#define SEATS_ERR_SEAT_IS_RESERVED   2
 #define SEATS_ERR_SEAT_NOT_RESERVED  3
 
-/**
- * Call only after a fork()
- */
 void setup_seats();
 
-/**
- * Return 1 if the seat is free, false otherwise.
- * There is no guarantee the seat will remain free if immediately
- * followed by a call to book_set
- */
 bool is_seat_free(int seat_num);
 
-/**
- * Try to reserve a given seat for a given client.
- * @return 0 if successful as the seat was available and valid,
- *         otherwise an error code.
- */
-int book_seat(int seat_num, int client_id)
+int book_seat(int seat_num, int client_id);
 
-/**
- * Free a previously reserved seat.
- * @return 0 if successfully freed,
- *         otherwise an error code.
- */
 int free_seat(int seat_num);
-
-/**
- * @return true if no seats have been reserved (empty house)
- */
-bool empty_house();
-
-/**
- * @return true if there are no available seats (full house)
- */
-bool full_house();
 
 #endif // SEATS_H___
